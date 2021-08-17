2f7bcf8f4e31f14dc8e34dc6fafe9dda99100d4b
Viglot <vigviglot36.hiltunen@gmail.com>
2021-08-17 09:21:01 +0300

    start

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector('textarea').addEventListener('change', function (event){
+    console.log(event.target.value);
+})
+
+const textarea = ''
+ 
+
+console.log(`${textarea.length} ${textarea.length}`);
+
