131f294fe28d79a326dd2ec2c6a17726f22ed191
Viglot <vigviglot36.hiltunen@gmail.com>
2021-08-31 10:48:31 +0300

    ga

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ document.querySelector('Textarea').addEventListener('input', function (event) {
 
     let Text = event.target.value.toString();
 
+
     document.getElementById('tecken').innerHTML = "tecken: " + Text.length
+
     document.getElementById('ord').innerHTML = "ord: " + Text.match( /(\w+)/g ).length
 
+    function countWords(str) {
+        str = str.replace(/(^\s*)|(\s*$)/gi,"");
+        str = str.replace(/[ ]{2,}/gi," ");
+        str = str.replace(/\n /,"\n");
+        return str.split(' ').length;
+      }
 });
